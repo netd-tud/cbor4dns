742ec928fa18304f9ebcd10752a18668c0959afa
Martine Lenders <m.lenders@fu-berlin.de>
2023-05-05 18:59:10 +0200

    trie: add counting tries

--- cbor2_dns/trie.py
+++ cbor2_dns/trie.py
@@ -3,8 +3,9 @@ Provides the trie data structure.
 """
 
 import enum
+import itertools
+import re
 import pprint
-import typing
 
 
 class TrieSearchResult(enum.Enum):
@@ -16,7 +17,47 @@ class TrieSearchResult(enum.Enum):
     MATCH_FOUND = 2
 
 
-class StringTrie:
+class TrieNode(dict):
+    def iter_children(self):
+        for k in self.keys():
+            yield k
+
+    def get_child(self, child):
+        return self[child]
+
+    def add_child(self, child):
+        return self.setdefault(child, type(self)())
+
+    def set_child(self, child, subtree):
+        self[child] = subtree
+        return subtree
+
+    def has_child(self, child):
+        return child in self
+
+    def remove_child(self, child):
+        del self[child]
+
+
+class CountingTrieNode(TrieNode):
+    def __init__(self):
+        self.count = 0
+
+    def add_child(self, child):
+        res = super().add_child(child)
+        res.count += 1
+        return res
+
+    def remove_child(self, child):
+        if hasattr(self[child], "count"):
+            self[child].count -= 1
+            if not self[child].count:
+                super().remove_child(child)
+        else:
+            super().remove_child(child)
+
+
+class BaseTrie:
     """A trie
 
     Based on https://stackoverflow.com/a/11016430
@@ -27,9 +68,10 @@ class StringTrie:
     """
     _end_marker = None
     _trie_type = str
+    _trie_node_class = TrieNode
 
     def __init__(self, strings=None):
-        self.root = {}
+        self.root = self._trie_node_class()
         if strings:
             for string in strings:
                 self.insert(string)
@@ -58,7 +100,7 @@ class StringTrie:
         for char in string:
             if char not in current_node:
                 return TrieSearchResult.NOT_FOUND
-            current_node = current_node[char]
+            current_node = current_node.get_child(char)
         if self._end_marker in current_node:
             return TrieSearchResult.MATCH_FOUND
         return TrieSearchResult.PREFIX_FOUND
@@ -76,25 +118,21 @@ class StringTrie:
             raise TypeError(f"string={string} must be {self._trie_type.__name__}")
         current_node = self.root
         for char in string:
-            current_node = current_node.setdefault(char, {})
-        current_node[self._end_marker] = {}
+            current_node = current_node.add_child(char)
+        current_node.add_child(self._end_marker)
 
     def _remove(self, node, string, depth=0):
-        if not node:
-            return None
         if depth == len(string):
-            if self._end_marker in node:
-                del node[self._end_marker]
+            if node.has_child(self._end_marker):
+                node.remove_child(self._end_marker)
             if not node:
-                del node
                 node = None
             return node
         key = string[depth]
-        node[key] = self._remove(node[key], string, depth + 1)
-        if not node[key]:
-            del node[key]
+        node.set_child(key, self._remove(node.get_child(key), string, depth + 1))
+        if not node.get_child(key):
+            node.remove_child(key)
         if not node:
-            del node
             node = None
         return node
 
@@ -102,5 +140,21 @@ class StringTrie:
         self._remove(self.root, string)
 
 
-class BytesTrie(StringTrie):
+class StringTrie(BaseTrie):
+    pass
+
+
+class BytesTrie(BaseTrie):
     _trie_type = bytes
+
+
+class CountingBaseTrie(BaseTrie):
+    _trie_node_class = CountingTrieNode
+
+
+class CountingStringTrie(StringTrie, CountingBaseTrie):
+    pass
+
+
+class CountingBytesTrie(BytesTrie, CountingBaseTrie):
+    pass
